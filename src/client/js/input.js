d7ea0031dbd4425cb51df1600202541935694f1d
Mason Chen <b08901058@ntu.edu.tw>
2020-09-25 00:03:00 +0800

    client side update

--- src/client/js/input.js
+++ src/client/js/input.js
@@ -1,7 +1,7 @@
 // Learn more about this file at:
 // https://victorzhou.com/blog/build-an-io-game-part-1/#6-client-input-%EF%B8%8F
 import { updateDirection } from "./networking";
-
+import { updateMovement } from "./networking";
 function handleInput(x, y) {
   const dir = Math.atan2(x - window.innerWidth / 2, window.innerHeight / 2 - y);
   updateDirection(dir);
@@ -16,11 +16,22 @@ function onTouchInput(e) {
   handleInput(touch.clientX, touch.clientY);
 }
 
+function onKeydown(e) {
+  updateMovement(["keydown", e.code]);
+}
+
+function onKeyUp(e){
+  updateMovement(["keyup", e.code]);
+}
+
 export function startCapturingInput() {
   window.addEventListener("mousemove", onMouseInput);
   window.addEventListener("click", onMouseInput);
   window.addEventListener("touchstart", onTouchInput);
   window.addEventListener("touchmove", onTouchInput);
+  window.addEventListener("keydown", onKeydown);
+  window.addEventListener("keyup", onKeyUp);
+  //window.addEventListener("keypress", onKeyPress);
 }
 
 export function stopCapturingInput() {
